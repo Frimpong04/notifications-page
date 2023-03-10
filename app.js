282a362f1d2283eb7e0c9da16941ff3683ac8f7e
Frimpong Boateng <executivekfb27@gmail.com>
2023-03-10 13:05:32 +0000

    add javascript to link the notification number to the toggling of the divs

--- app.js
+++ app.js
@@ -5,19 +5,24 @@ const notificationDiv = document.getElementById("notification-items-container").
 const unreadMark = document.getElementsByClassName("unread-mark");
 
 // global variables
-let count = notificationDiv.length;
+let count;
+count = notificationDiv.length;
 notificationCount.innerText = count;
 console.log(notificationDiv);
 
 // functions
 function reset() {
     console.log("reset to 0");
-    notificationCount.innerText = 0; 
+    
     console.log(unreadMark);
     for (element of unreadMark) {
         element.style.visibility = "hidden";
     }
-    
+    for (div of notificationDiv) {
+        div.classList.add("notification-item-clicked");
+    }
+    count = 0;
+    notificationCount.innerText = count;
 }
 
 function marked(item) {
@@ -26,10 +31,13 @@ function marked(item) {
     
     if (item.classList.contains("notification-item-clicked")) {
         markSpan.style.visibility = "hidden";
+        console.log(count -= 1);
+        notificationCount.innerText = count;
     
     } else {
         markSpan.style.visibility = "visible";
-
+        console.log(count += 1);
+        notificationCount.innerText = count;
     }
 
 }
